00a5e204c17ddacd2b96960d074451c2264d9e01
Burke Webster <burke.webster@gmail.com>
2025-06-19 09:56:19 -0500

    Expand spec

--- spec/requests/api/tracking_events_spec.rb
+++ spec/requests/api/tracking_events_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 RSpec.describe "Api::TrackingEvents", type: :request do
   describe "POST /api/tracking_events" do
     it "returns http success" do
+      allow(ProcessTrackingEventJob).to receive(:perform_later)
+
       headers = { "CONTENT_TYPE" => "application/json" }
       payload = {
         "id" => "abc123",
@@ -13,9 +15,24 @@ RSpec.describe "Api::TrackingEvents", type: :request do
 
       expect do
         post "/api/tracking_events", params: payload.to_json, headers: headers
-      end.to change(TrackingEvent, :count).by(1)
+      end.to change(TrackingEvent, :count).by(1).and change(RfidTag, :count).by(1)
 
       expect(response).to have_http_status(:success)
+
+      tracking_event = TrackingEvent.last
+      expect(tracking_event).to have_attributes(
+        scanned_at: be_within(1.second).of(Time.zone.now),
+        submitted_location: eq("3"),
+        location_id: be_nil,
+        rfid_tag: have_attributes(
+          tag_id: "abc123"
+        )
+      )
+
+      expect(ProcessTrackingEventJob)
+        .to have_received(:perform_later)
+        .once
+        .with(tracking_event_id: tracking_event.id)
     end
   end
 end
