aa796497ec4a812116d7272da2814c526a8fe89d
Burke Webster <burke.webster@gmail.com>
2025-06-19 09:40:39 -0500

    Get rubocop passing

--- spec/requests/api/tracking_events_spec.rb
+++ spec/requests/api/tracking_events_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe "Api::TrackingEvents", type: :request do
         "loc" => "3"
       }
 
-      expect {
+      expect do
         post "/api/tracking_events", params: payload.to_json, headers: headers
-      }.to change(TrackingEvent, :count).by(1)
+      end.to change(TrackingEvent, :count).by(1)
 
       expect(response).to have_http_status(:success)
     end
