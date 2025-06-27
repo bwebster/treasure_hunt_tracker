a32581e1b7bfac9e3d43253d950f11e365d88e69
Burke Webster <burke.webster@gmail.com>
2025-06-27 08:32:48 -0500

    Ability to copy locations

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   end
 
   resources :events do
+    patch :copy_locations, on: :member
+
     resources :locations, only: %i[new create edit update destroy]
   end
 
