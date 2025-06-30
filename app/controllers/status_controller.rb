6abab056a25cb4de227ae8447c0c8b06a370f133
Burke Webster <burke.webster@gmail.com>
2025-06-30 15:55:07 -0500

    expand status view

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -24,5 +24,14 @@ class StatusController < AdminController
         received: health_check.received
       )
     end
+
+    @location_status = current_locations.values.collect do |location|
+      Status.new(
+        location: location,
+        received: health_checks
+                    .detect { |hc| hc.location == location.number.to_s }
+                    &.received
+      )
+    end.sort_by { |status| status.location.name }
   end
 end
